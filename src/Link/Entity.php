0e639636cb2c966af099f35b6030964352f4b790
Vít Kutný <vitkutny@icloud.com>
2015-05-27 21:27:35 +0200

    dev

--- src/Link/Entity.php
+++ src/Link/Entity.php
@@ -9,8 +9,6 @@ use Ytnuk;
  * @property string $destination
  * @property Nextras\Orm\Relationships\OneHasMany|Alias\Entity[] $aliases {1:m Alias\Repository $link}
  * @property Nextras\Orm\Relationships\OneHasMany|Parameter\Entity[] $parameters {1:m Parameter\Repository $link}
- * TODO:
- * @property Nextras\Orm\Relationships\OneHasOneDirected|Ytnuk\Menu\Entity|NULL $menu {1:1d Ytnuk\Menu\Repository $link}
  */
 class Entity extends Ytnuk\Orm\Entity
 {
