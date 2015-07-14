228138e39cb69ea90ec38ef7e26257742be0c817
Vít Kutný <vitkutny@icloud.com>
2015-07-14 19:29:41 +0200

    dev

--- src/Link/Entity.php
+++ src/Link/Entity.php
@@ -1,16 +1,31 @@
 <?php
-
 namespace Ytnuk\Link;
 
 use Nextras;
 use Ytnuk;
 
 /**
- * @property string $destination
+ * @property-read string $destination {virtual}
+ * @property string $module
+ * @property string $presenter {default Presenter}
+ * @property string $action {default view}
  * @property Nextras\Orm\Relationships\OneHasMany|Alias\Entity[] $aliases {1:m Alias\Repository $link}
  * @property Nextras\Orm\Relationships\OneHasMany|Parameter\Entity[] $parameters {1:m Parameter\Repository $link}
  */
 class Entity extends Ytnuk\Orm\Entity
 {
 
+	/**
+	 * @param bool $fullyQualified
+	 *
+	 * @return string
+	 */
+	public function getterDestination($fullyQualified = TRUE)
+	{
+		return ($fullyQualified ? ':' : NULL) . implode(':', [
+			$this->module,
+			$this->presenter,
+			$this->action
+		]);
+	}
 }
