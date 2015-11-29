511fb1f60bd45db1903c6c835cf1208b016c80c6
Vít Kutný <vitkutny@icloud.com>
2015-11-29 17:18:02 +0100

    Link\Alias locale

--- src/Link/Alias/Entity.php
+++ src/Link/Alias/Entity.php
@@ -6,8 +6,9 @@ use Ytnuk;
 
 /**
  * @property int $id {primary}
- * @property string $value
  * @property Nextras\Orm\Relationships\ManyHasOne|Ytnuk\Link\Entity $link {m:1 Ytnuk\Link\Entity::$aliases}
+ * @property Nextras\Orm\Relationships\ManyHasOne|Ytnuk\Translation\Locale\Entity $locale {m:1 Ytnuk\Translation\Locale\Entity::$linkAliases}
+ * @property string $value
  * @property bool|NULL $primary
  */
 final class Entity
